563e0bd480913b5a8be87b17e91eacba5618a1fc
jilg <johannes.zilg@gmail.com>
2018-10-04 23:26:08 +0200

    use unix cp function to check for existing files and verbose

--- install.sh
+++ install.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-copyFiles() {
-    cp ./src/vimrc ~/.vimrc
-    printf "\n.vimrc copied"
-    cp -r ./src/vim ~/.vim
-    printf "\n.vim/ copied"
-}
+cp -iv ./src/vimrc ~/.vimrc
+cp -riv ./src/vim ~/.vim
 
-if [ -e ~/.vimrc ] || [ -d ~/.vim ]
-then
-    read -p "Overwrite existing settings (y/n)? " -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        copyFiles
-    fi
-else
-    copyFiles
-fi
